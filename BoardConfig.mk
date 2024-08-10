ac0b4ad2672bcac0cd1ec3d92e898528f027ab40
kamikaonashi <thorschi93@gmx.at>
2024-08-10 09:58:05 +0200

    mido: add public sepolicy folder

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -151,6 +151,7 @@ VENDOR_SECURITY_PATCH := "2024-04-05"
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
