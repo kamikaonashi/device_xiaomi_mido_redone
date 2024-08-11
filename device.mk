a6ad3995ef06cd411b02f92521df6a9cc76912c8
mikeNG <1216752+mikeNG@users.noreply.github.com>
2024-08-11 07:28:07 +0200

    mido: build Lineage Health HAL

--- device.mk
+++ device.mk
@@ -327,6 +327,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service.xiaomi_mido
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_8953_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
