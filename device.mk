a532fdd9c805b47f6b3bb426d42684125e756a88
ZeeLog <Danjel.Zammit@gmail.com>
2024-08-18 09:15:17 +0200

    mido: Nuke Moto Dolby Atmos
    * in preparation for Xiaomi Dolby Atmos generic implementation
    
    Revert "mido: Add missing stagefright libraries"
    
    This reverts commit f678765840b4238f3165fb415a115aa40b0d68da.
    
    Revert "mido: Import Moto Dolby Atmos"
    
    This reverts commit dd4a42bc20227145002a6546c03ae6950153c601.

--- device.mk
+++ device.mk
@@ -202,18 +202,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
-# Dolby
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
-
-TARGET_EXCLUDES_AUDIOFX := true
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
-    $(LOCAL_PATH)/configs/sysconfig/preinstalled-packages-platform-xiaomi-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-xiaomi-product.xml \
-    prebuilts/vndk/v33/arm/arch-arm-armv7-a-neon/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstagefright_foundation-v33.so \
-    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation-v33.so
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
@@ -356,15 +344,8 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
-    libopus.vendor \
-    libstagefright_amrnb_common.vendor \
-    libstagefright_enc_common \
-    libstagefright_enc_common.vendor \
     libstagefrighthw \
-    libstagefright_softomx_plugin.vendor \
-    libstagefright_softomx.vendor \
-    libvorbisidec.vendor \
-    libvpx.vendor
+    libstagefright_enc_common
 
 # Power
 PRODUCT_PACKAGES += \
