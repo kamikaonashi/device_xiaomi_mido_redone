61f58adb503ddcd3627fb359049e4c9bc0c87888
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-08-19 22:57:10 +0200

    mido: Move to Xiaomi IR AIDL
    Change-Id: I46094bcbfaba252b5d98ea8211b927fdaa079e2d | AOSP

--- device.mk
+++ device.mk
@@ -181,7 +181,7 @@ PRODUCT_PACKAGES += \
 
 # Consumerir
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-service.xiaomi_mido
+    android.hardware.ir-service.xiaomi
 
 # Display
 PRODUCT_PACKAGES += \
