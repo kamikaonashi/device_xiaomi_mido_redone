3452fcba8dab7d444bfe12666a4d0e74c8d35d80
Bruno Martins <bgcngm@gmail.com>
2024-09-03 12:16:30 +0200

    mido: rootdir: Delete class_main init shell script
    Apart from useless battery capacity faking, all this does is to
    start/stop data and radio services based upon the baseband type
    of several QCOM targets. Since all the supported targets in this
    common tree are of the same type, simply start all relevant services
    unconditionally and get rid of one more shell script.
    
    Change-Id: I1297b2537fcbea31d514bab5e248691cabdf523c | AOSP

--- device.mk
+++ device.mk
@@ -378,7 +378,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.zram \
-    init.class_main.sh \
     init.qcom.post_boot.sh \
     init.qcom.sensors.sh \
     init.qcom.rc \
