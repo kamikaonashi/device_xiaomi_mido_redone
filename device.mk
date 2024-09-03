0c5ff2cb6dc48a1c743eae5261a766723c6781e0
ZeeLog <Danjel.Zammit@gmail.com>
2024-09-03 12:28:08 +0200

    mido: Ditch init.qcom.sensors script

--- device.mk
+++ device.mk
@@ -379,7 +379,6 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.zram \
     init.qcom.post_boot.sh \
-    init.qcom.sensors.sh \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
