5211fc7cb5952cd22820643d9a84fe8574ded7bf
Michael Bestas <mkbestas@lineageos.org>
2024-09-21 11:30:54 +0200

    mido: Sync extract scripts with templates

--- extract-files.sh
+++ extract-files.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
-
-if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
-    return
-fi
+#
 
 set -e
 
-# Required!
 DEVICE=mido
 VENDOR=xiaomi
 
@@ -21,9 +16,13 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../.."
 
+# If XML files don't have comments before the XML header, use this flag
+# Can still be used with broken XML files by using blob_fixup
+export TARGET_DISABLE_XML_FIXING=true
+
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
 source "${HELPER}"
@@ -36,19 +35,20 @@ SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
-        -n | --no-cleanup )
-                CLEAN_VENDOR=false
-                ;;
-        -k | --kang )
-                KANG="--kang"
-                ;;
-        -s | --section )
-                SECTION="${2}"; shift
-                CLEAN_VENDOR=false
-                ;;
-        * )
-                SRC="${1}"
-                ;;
+        -n | --no-cleanup)
+            CLEAN_VENDOR=false
+            ;;
+        -k | --kang)
+            KANG="--kang"
+            ;;
+        -s | --section)
+            SECTION="${2}"
+            shift
+            CLEAN_VENDOR=false
+            ;;
+        *)
+            SRC="${1}"
+            ;;
     esac
     shift
 done
@@ -57,60 +57,113 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        # Camera configs
+        vendor/lib/libmmcamera2_sensor_modules.so)
+            [ "$2" = "" ] && return 0
+            sed -i "s|/system/etc/camera|/vendor/etc/camera|g" "${2}"
+            ;;
+        # Camera socket
+        vendor/bin/mm-qcamera-daemon)
+            [ "$2" = "" ] && return 0
+            sed -i "s|/data/misc/camera/cam_socket|/data/vendor/qcam/cam_socket|g" "${2}"
+            ;;
+        # Camera data
+        vendor/lib/libmmcamera2_cpp_module.so \
+        | libmmcamera2_dcrf.so \
+        | libmmcamera2_iface_modules.so \
+        | libmmcamera2_imglib_modules.so \
+        | libmmcamera2_mct.so \
+        | libmmcamera2_pproc_modules.so \
+        | libmmcamera2_q3a_core.so \
+        | libmmcamera2_sensor_modules.so \
+        | libmmcamera2_stats_algorithm.so \
+        | libmmcamera2_stats_modules.so \
+        | libmmcamera_dbg.so \
+        | libmmcamera_imglib.so \
+        | libmmcamera_pdafcamif.so \
+        | libmmcamera_pdaf.so \
+        | libmmcamera_tintless_algo.so \
+        | libmmcamera_tintless_bg_pca_algo.so \
+        | libmmcamera_tuning.so)
+            [ "$2" = "" ] && return 0
+            sed -i "s|/data/misc/camera/|/data/vendor/qcam/|g" "${2}"
+            ;;
+        # Camera debug log file
+        vendor/lib/libmmcamera_dbg.so)
+            [ "$2" = "" ] && return 0
+            sed -i "s|persist.camera.debug.logfile|persist.vendor.camera.dbglog|g" "${2}"
+            ;;
+        # Camera graphicbuffer shim
+        vendor/lib/libmmcamera_ppeiscore.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
+            ;;
+        # Camera VNDK support
+        vendor/lib/libmmcamera2_stats_modules.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
+            "${PATCHELF}" --remove-needed "libgui.so" "${2}"
+            sed -i "s|libandroid.so|libcamshim.so|g" "${2}"
+            ;;
+        vendor/lib/libmmcamera_ppeiscore.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --remove-needed "libgui.so" "${2}"
+            ;;
+        vendor/lib/libmpbase.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
+            ;;
+        # Dolby
+        vendor/lib/libstagefright_soft_ddpdec.so | libstagefright_soft_ac4dec.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
+        vendor/lib64/libdlbdsservice.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
+        # Goodix
+        vendor/bin/gx_fpd)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --remove-needed "libunwind.so" "${2}"
+            "${PATCHELF}" --remove-needed "libbacktrace.so" "${2}"
+            "${PATCHELF}" --add-needed "libshims_gxfpd.so" "${2}"
+            "${PATCHELF}" --add-needed "fakelogprint.so" "${2}"
+            ;;
+        vendor/lib64/hw/fingerprint.goodix.so | gxfingerprint.default.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "fakelogprint.so" "${2}"
+            ;;
+        # IMS
+        system_ext/lib64/lib-imsvideocodec.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
+            ;;
+        # RIL
+        vendor/lib64/libril-qc-hal-qmi.so)
+            [ "$2" = "" ] && return 0
+            for v in 1.{0..2}; do
+                sed -i "s|android.hardware.radio.config@${v}.so|android.hardware.radio.c_shim@${v}.so|g" "${2}"
+            done
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
+}
+
 # Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 
-extract "${MY_DIR}"/proprietary-files.txt "${SRC}" \
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
         "${KANG}" --section "${SECTION}"
-
-DEVICE_BLOB_ROOT="${ANDROID_ROOT}"/vendor/"${VENDOR}"/"${DEVICE}"/proprietary
-
-# Camera configs
-sed -i "s|/system/etc/camera|/vendor/etc/camera|g" "${DEVICE_BLOB_ROOT}"/vendor/lib/libmmcamera2_sensor_modules.so
-
-# Camera socket
-sed -i "s|/data/misc/camera/cam_socket|/data/vendor/qcam/cam_socket|g" "${DEVICE_BLOB_ROOT}"/vendor/bin/mm-qcamera-daemon
-
-# Camera data
-for CAMERA_LIB in libmmcamera2_cpp_module.so libmmcamera2_dcrf.so libmmcamera2_iface_modules.so libmmcamera2_imglib_modules.so libmmcamera2_mct.so libmmcamera2_pproc_modules.so libmmcamera2_q3a_core.so libmmcamera2_sensor_modules.so libmmcamera2_stats_algorithm.so libmmcamera2_stats_modules.so libmmcamera_dbg.so libmmcamera_imglib.so libmmcamera_pdafcamif.so libmmcamera_pdaf.so libmmcamera_tintless_algo.so libmmcamera_tintless_bg_pca_algo.so libmmcamera_tuning.so; do
-    sed -i "s|/data/misc/camera/|/data/vendor/qcam/|g" "${DEVICE_BLOB_ROOT}"/vendor/lib/${CAMERA_LIB}
-done
-
-# Camera debug log file
-sed -i "s|persist.camera.debug.logfile|persist.vendor.camera.dbglog|g" "${DEVICE_BLOB_ROOT}"/vendor/lib/libmmcamera_dbg.so
-
-# Camera graphicbuffer shim
-"${PATCHELF}" --add-needed "libui_shim.so" "${DEVICE_BLOB_ROOT}"/vendor/lib/libmmcamera_ppeiscore.so
-
-# Camera VNDK support
-"${PATCHELF}" --remove-needed "libandroid.so" "${DEVICE_BLOB_ROOT}"/vendor/lib/libmmcamera2_stats_modules.so
-"${PATCHELF}" --remove-needed "libgui.so" "${DEVICE_BLOB_ROOT}"/vendor/lib/libmmcamera2_stats_modules.so
-sed -i "s|libandroid.so|libcamshim.so|g" "${DEVICE_BLOB_ROOT}"/vendor/lib/libmmcamera2_stats_modules.so
-"${PATCHELF}" --remove-needed "libgui.so" "${DEVICE_BLOB_ROOT}"/vendor/lib/libmmcamera_ppeiscore.so
-"${PATCHELF}" --remove-needed "libandroid.so" "${DEVICE_BLOB_ROOT}"/vendor/lib/libmpbase.so
-
-# Dolby
-"${PATCHELF} --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${DEVICE_BLOB_ROOT}"/vendor/lib/libstagefright_soft_ddpdec.so"
-"${PATCHELF} --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${DEVICE_BLOB_ROOT}"/vendor/lib/libstagefright_soft_ac4dec.so"
-"${PATCHELF} --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${DEVICE_BLOB_ROOT}"/vendor/lib64/libdlbdsservice.so"
-
-# Gnss
-sed -i -e '$a\\    capabilities NET_BIND_SERVICE' "${DEVICE_BLOB_ROOT}"/vendor/etc/init/android.hardware.gnss@2.1-service-qti.rc
-
-# Goodix
-"${PATCHELF}" --remove-needed "libunwind.so" "${DEVICE_BLOB_ROOT}"/vendor/bin/gx_fpd
-"${PATCHELF}" --remove-needed "libbacktrace.so" "${DEVICE_BLOB_ROOT}"/vendor/bin/gx_fpd
-"${PATCHELF}" --add-needed "libshims_gxfpd.so" "${DEVICE_BLOB_ROOT}"/vendor/bin/gx_fpd
-"${PATCHELF}" --add-needed "fakelogprint.so" "${DEVICE_BLOB_ROOT}"/vendor/bin/gx_fpd
-"${PATCHELF}" --add-needed "fakelogprint.so" "${DEVICE_BLOB_ROOT}"/vendor/lib64/hw/fingerprint.goodix.so
-"${PATCHELF}" --add-needed "fakelogprint.so" "${DEVICE_BLOB_ROOT}"/vendor/lib64/hw/gxfingerprint.default.so
-
-# IMS
-"${PATCHELF}" --add-needed "libgui_shim.so" "${DEVICE_BLOB_ROOT}"/system_ext/lib64/lib-imsvideocodec.so
-
-# RIL
-for v in 1.{0..2}; do
-    sed -i "s|android.hardware.radio.config@${v}.so|android.hardware.radio.c_shim@${v}.so|g" "${DEVICE_BLOB_ROOT}"/vendor/lib64/libril-qc-hal-qmi.so"
-done
 
 "${MY_DIR}/setup-makefiles.sh"
