39559cea5d78e1217852a5ed5edbc8e0a1d67b0a
Michael Bestas <mkbestas@lineageos.org>
2024-10-30 19:30:44 +0100

    mido: adjust override prop key names

--- lineage_mido.mk
+++ lineage_mido.mk
@@ -36,7 +36,5 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V9.6.1.0.NCFMIFD release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/mido/mido:7.0/NRD90M/V9.6.1.0.NCFMIFD:user/release-keys"
+    BuildDesc="mido-user 7.0 NRD90M V9.6.1.0.NCFMIFD release-keys" \
+    BuildFingerprint="xiaomi/mido/mido:7.0/NRD90M/V9.6.1.0.NCFMIFD:user/release-keys"
